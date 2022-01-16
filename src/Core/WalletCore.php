f20092014a11aafd44027d6271a6df5591684b11
fshangala <fshangala@gmail.com>
2022-01-16 09:05:22 -0800

    wallet complete

--- src/Core/WalletCore.php
+++ src/Core/WalletCore.php
@@ -36,6 +36,51 @@ class WalletCore
         }
     }
 
+    public function withdrawRequest($user_id,$currency,$credit)
+    {
+        try {
+            $wallet = Wallet::findOrFail($user_id);
+            $instance = Transaction::create([
+                "wallet_id"=>$wallet->id,
+                "currency"=>$currency,
+                "debit"=>0,
+                "credit"=>$credit,
+                "reference"=>"Withdraw request",
+                "status"=>"pending"
+            ]);
+            return $instance;
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+    public function transfer($user_id,$wallet_id,$currency,$amount,$reference)
+    {
+        try {
+            $instance = Wallet::findOrFail($user_id);
+            $credit = Transaction::create([
+                "wallet_id"=>$instance->id,
+                "currency"=>$currency,
+                "debit"=>0,
+                "credit"=>$amount,
+                "reference"=>$reference,
+                "status"=>"approved"
+            ]);
+            $debit = Transaction::create([
+                "wallet_id"=>$wallet_id,
+                "currency"=>$currency,
+                "debit"=>$amount,
+                "credit"=>0,
+                "reference"=>$reference,
+                "status"=>"pending"
+            ]);
+            
+            return $debit;
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
     public function approveTransaction($transaction_id)
     {
         try {
@@ -44,6 +89,71 @@ class WalletCore
                 "status"=>"approved"
             ]);
             return $instance;
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+    public function transactionHistory($user_id)
+    {
+        try {
+            $wallet = Wallet::findOrFail($user_id);
+            $instances = Transaction::where("wallet_id",$wallet->id)->get();
+            $wallet["transaction_history"] = $instances;
+            return $wallet;
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+    public function getTransaction($transaction_id)
+    {
+        try {
+            $instance = Transaction::findOrFail($transaction_id);
+            return $instance;
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+    public function totalBalance()
+    {
+        try {
+            $instances = Transaction::all();
+            if(count($instances)>0){
+                $debit = 0;
+                $credit = 0;
+                foreach ($instances as $instance) {
+                    $debit = $debit + $instance["debit"];
+                    $credit = $credit + $instance["credit"];
+                }
+            } else {
+                $debit = 0;
+                $credit = 0;
+            }
+            return ["debit"=>$debit,"credit"=>$credit];
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+    public function totalWalletBalance($user_id)
+    {
+        try {
+            $wallet = Wallet::findOrFail($user_id);
+            $instances = Transaction::where("wallet_id",$wallet->id)->get();
+            if(count($instances)>0){
+                $debit = 0;
+                $credit = 0;
+                foreach ($instances as $instance) {
+                    $debit = $debit + $instance["debit"];
+                    $credit = $credit + $instance["credit"];
+                }
+            } else {
+                $debit = 0;
+                $credit = 0;
+            }
+            return ["debit"=>$debit,"credit"=>$credit];
         } catch (Exception $e) {
             throw $e;
         }
